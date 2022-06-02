7234483f72c0e69d5b5ff93d854060dfb579ffde
Laurent Chau <minh-phuoc.chau@efrei.net>
2022-06-02 09:41:12 +0200

    fix form & table

--- js/script.js
+++ js/script.js
@@ -17,11 +17,17 @@ function ajouter() {
     const newItem = document.createElement("tr");
     const nameTd = document.createElement("td");
     const mailTd = document.createElement("td");
+    const responseTd = document.createElement("td");
 
     nameTd.textContent = document.form.name.value;
     mailTd.textContent = document.form.mail.value;
+    responseTd.textContent = document.form.reponse.value;
 
-    newItem.append(nameTd, mailTd);
+    nameTd.setAttribute("data-label", "Nom");
+    mailTd.setAttribute("data-label", "Adresse mail");
+    responseTd.setAttribute("data-label", "Réponse");
+
+    newItem.append(nameTd, mailTd, responseTd);
 
     const table = document.querySelector("table tbody");
     table.appendChild(newItem);
